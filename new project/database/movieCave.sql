15c8eaddcd7a4679caed550c8275ac9670314628
uzairmazhar <uzair.mazhar195@gmail.com>
2019-06-27 18:26:34 +0500

    trailer table added and showed covers of trailers in trailerinder.php

--- new project/database/movieCave.sql
+++ new project/database/movieCave.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2019 at 6:52 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.1
+-- Generation Time: Jun 27, 2019 at 01:56 PM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `movieCave`
+-- Database: `moviecave`
 --
 
 -- --------------------------------------------------------
@@ -45,8 +45,6 @@ INSERT INTO `admins` (`user_id`, `user_email`, `user_pass`) VALUES
 
 -- --------------------------------------------------------
 
-
-
 --
 -- Table structure for table `categories`
 --
@@ -69,10 +67,8 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 
 -- --------------------------------------------------------
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `products`
+-- Table structure for table `movies`
 --
 
 CREATE TABLE `movies` (
@@ -85,12 +81,12 @@ CREATE TABLE `movies` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `products`
+-- Dumping data for table `movies`
 --
 
 INSERT INTO `movies` (`mov_id`, `mov_cat`, `mov_title`, `mov_desc`, `mov_image`, `mov_keywords`) VALUES
-(1, 1, 'John Wick: Chapter 3 – Parabellum', 'After gunning down a member of the High Table -- the shadowy international assassin''s guild -- legendary hit man John Wick finds himself stripped of the organization''s protective services. Now stuck with a $14 million bounty on his head, Wick must fight his way through the streets of New York as he becomes the target of the world''s most ruthless killers.', 'img_john_wick.jpg', 'john, j, wick'),
-(2, 1, 'Black Panther', 'After the death of his father, T''Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T''Challa''s mettle as king -- and as Black Panther -- gets tested when he''s drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.', 'img_black_panther.jpg', 'b, black, pan, panther'),
+(1, 1, 'John Wick: Chapter 3 – Parabellum', 'After gunning down a member of the High Table -- the shadowy international assassin\'s guild -- legendary hit man John Wick finds himself stripped of the organization\'s protective services. Now stuck with a $14 million bounty on his head, Wick must fight his way through the streets of New York as he becomes the target of the world\'s most ruthless killers.', 'img_john_wick.jpg', 'john, j, wick'),
+(2, 1, 'Black Panther', 'After the death of his father, T\'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T\'Challa\'s mettle as king -- and as Black Panther -- gets tested when he\'s drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.', 'img_black_panther.jpg', 'b, black, pan, panther'),
 (3, 2, 'Inception', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', 'img_inception.jpg', 'i, incep, inception'),
 (4, 2, 'Sherlock Holmes', 'Detective Sherlock Holmes and his stalwart partner Watson engage in a battle of wits and brawn with a nemesis whose plot is a threat to all of England.', 'img_sherlock.jpg', 'she, s, sherl, sherlock'),
 (5, 3, 'Home Alone', 'An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.', 'img_halone.jpg', 'h, hom, home, alone'),
@@ -99,6 +95,30 @@ INSERT INTO `movies` (`mov_id`, `mov_cat`, `mov_title`, `mov_desc`, `mov_image`,
 (8, 4, 'The Dark Knight', 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 'img_dark.jpg', 'dar, dark, kni, knight'),
 (9, 5, 'Saw II', 'A detective and his team must rescue 8 people trapped in a factory by the twisted serial killer known as Jigsaw.', 'img_saw.jpg', 'sa, saw, two'),
 (10, 5, 'Insidious', 'A family looks to prevent evil spirits from trapping their comatose child in a realm called The Further.', 'img_insd.jpg', 'ins, insidious, dious');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trailers`
+--
+
+CREATE TABLE `trailers` (
+  `tra_id` int(11) NOT NULL,
+  `tra_cat` int(11) NOT NULL,
+  `tra_title` varchar(255) NOT NULL,
+  `tra_desc` varchar(255) NOT NULL,
+  `tra_image` varchar(255) NOT NULL,
+  `tra_keywords` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `trailers`
+--
+
+INSERT INTO `trailers` (`tra_id`, `tra_cat`, `tra_title`, `tra_desc`, `tra_image`, `tra_keywords`) VALUES
+(1, '1', 'Spider-Man: Far From Home', 'Following the events of Avengers: Endgame, Spider-Man must step up to take on new threats in a world that has changed forever.', 'spider.jpg', 's,spider'),
+(2, '2', 'Lion King', 'Simba idolizes his father, King Mufasa, and takes to heart his own royal destiny on the plains of Africa. But not everyone in the kingdom celebrates the new cub s arrival. Scar, Mufasa s brother -- and former heir to the throne -- has plans of his own.', 'lion.jpg', 'l,lion,king'),
+(3, '1', 'Hobbs And Shaw', 'Ever since hulking lawman Hobbs (Johnson), a loyal agent of America s Diplomatic Security Service, and lawless outcast Shaw (Statham), a former British military elite operative, first faced off in 2015’s Furious 7, the duo have swapped smack talk and body', 'hobbs.jpg', 'hobbs,shaw,h');
 
 --
 -- Indexes for dumped tables
@@ -110,19 +130,23 @@ INSERT INTO `movies` (`mov_id`, `mov_cat`, `mov_title`, `mov_desc`, `mov_image`,
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`user_id`);
 
-
 --
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
-
 --
--- Indexes for table `products`
+-- Indexes for table `movies`
 --
 ALTER TABLE `movies`
   ADD PRIMARY KEY (`mov_id`);
+
+--
+-- Indexes for table `trailers`
+--
+ALTER TABLE `trailers`
+  ADD PRIMARY KEY (`tra_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -134,21 +158,23 @@ ALTER TABLE `movies`
 ALTER TABLE `admins`
   MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
-
-
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
   MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
-
-
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
   MODIFY `mov_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `trailers`
+--
+ALTER TABLE `trailers`
+  MODIFY `tra_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
